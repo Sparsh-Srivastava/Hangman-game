f69b0c665c120247e7f3b9b6e88f5a4c07307312
raghav13901 <raghavkhatoria42@gmail.com>
2020-10-10 06:48:46 +0530

    Functionality changes

--- Hangman/index.js
+++ Hangman/index.js
@@ -1,10 +1,7 @@
 
 var questions = ["History's most expensive Goalkeeper","Signed his first contract on a napkin","Swedish professional footballer who plays as a striker for Serie A club Milan","A Chelsea legend in terms of his club goals","He would miss matches because he had a fear of flying","Real Madrid's right winger","Never got caught offside","Never got a red card","He used to touch the crossbar everytime real madrid scored a goal","He holds the record for most clean sheets kept in a single season, with 24"]; //Array of questions
-
 var hint = ["£71.6m move from Athletic Bilbao to Chelsea","FC Barcelona sporting director Carles Rexach was so impressed with his skills that he wanted to make a contract immediately and there was only a tissue paper.","At 10 years old he was used as a second half sub and scored 8 goals.","Not just a footballer in his home country, he helped stop a civil war in Ivory Coast after qualifying for the World Cup","Nicknamed the non-flying Dutchman ","Used to be a rugby player when he was young but changed to football","Real Madrid's current manager","German retired professional footballer who played as a right back or defensive midfielder","Popularly dubbed 'San Iker' for his ability to produce spectacular saves","He has the title of Premier League history's most clean sheets with 202"]; //Array of hints
-
 var answer = ["KEPA ARRIZABALAGA","LIONEL MESSI","ZLATAN IBRAHIMOVIC","DIDIER DROGBA","DENNIS BERGKAMP","GARETH BALE","ZINEDINE ZIDANE","PHILIPPE LAHM","IKER CASILLAS","PETR CECH"]; //Array of answers
-
 var random = Math.floor(Math.random()*questions.length); //Generate random number
 
 var selectedQ = questions[random]; //Selected question
@@ -22,6 +19,8 @@ var win=0;
 var space= [];
 
 var wrong= [];
+
+var SPc=['Tab','Control','Shift','Meta','Alt','Enter','Backspace',']','[',';',"'",'"',',','.','/'];
 
 var totalMistakes = 7;
 
@@ -60,50 +59,44 @@ for(var i=0; i < selectedA.length; i++)  //Generating dashes and "//" for spaces
 function enter(e)
 {
     
-    console.log(e.innerHTML);
-    
-    if(answer[random].indexOf(e.innerHTML) != -1)
-        {
-            for(var t=0; t< answer[random].length; t++)
-        {
-            if(e.innerHTML == selectedA[t])
-                {
-                    space[t]= e.innerHTML;
-                            document.getElementById("dash").innerHTML = space.join(" ");
-                                win+=1;
-                    if(win == selectedA.length-1)
-                        {
-                            document.getElementById("category").innerHTML = "Congratulations, YOU WON. Refresh to start again.";
-                                for(var z=0; z<26; z++)
-                                {
-                                    document.getElementsByClassName("key")[z].disabled = "true";
-                                }
-                        }
-                }
+    console.log(e.key);
+        if(SPc.indexOf(e.key)==-1   ){
+            if(answer[random].indexOf(e.key.toUpperCase()) != -1)
+            {
+                for(var t=0; t< answer[random].length; t++)
+            {
+                if(e.key.toUpperCase() == selectedA[t])
+                    {
+                        space[t]= e.key.toUpperCase();
+                                document.getElementById("dash").innerHTML = space.join(" ");
+                                    win+=1;
+                        
+                    }
 
+            }
         }
-        }
-    else
-        {
-            console.log("wrong");
-            lives +=1;
-            console.log(lives);
-            switch(lives){
-    case 1: line4();
-        break;
-    case 2: line5();
-        break;
-    case 3: line6();
-        break;
-    case 4: line7();
-        break;
-    case 5: line8();
-        break;
-    case 6: line9();
-        break;
-    case 7: line10();
-}
-        }
+        else
+            {
+                console.log("wrong");
+                lives +=1;
+                console.log(lives);
+                switch(lives){
+        case 1: line4();
+            break;
+        case 2: line5();
+            break;
+        case 3: line6();
+            break;
+        case 4: line7();
+            break;
+        case 5: line8();
+            break;
+        case 6: line9();
+            break;
+        case 7: line10();
+    }
+            }
+    }
 
 }
 
@@ -200,10 +193,6 @@ ctx.lineTo(220,100);
 ctx.stroke();  
 ctx.strokeStyle = "#900d0d";
 document.getElementById("category").innerHTML = "Better luck next time, Refresh to try again";
-    for(var z=0; z<26; z++)
-        {
-            document.getElementsByClassName("key")[z].disabled = "true";
-        }
     console.log(win);
 }
     
